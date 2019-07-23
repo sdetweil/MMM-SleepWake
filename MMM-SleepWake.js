90ab990f6f2a7b5a1c52bd70ed1041c28eff2e40
Sam Detweiler <sdetweil@gmail.com>
2019-07-23 08:51:44 -0500

    fix tabs

--- MMM-SleepWake.js
+++ MMM-SleepWake.js
@@ -2,101 +2,100 @@
 var v_self1;
 
 Module.register("MMM-SleepWake",{
-							previously_hidden: [],
-							defaults: {
-												delay: 15,
-												source: 'external',
-												mode:  'hide',
-												detectionDir: "/motion",
-												detectionFile: 'detected',
-							},
+	previously_hidden: [],
+	defaults: {
+		delay: 15,
+		source: "external",
+		mode:  "hide",
+		detectionDir: "/motion",
+		detectionFile: "detected",
+	},
 
-							socketNotificationReceived: function(notification, payload)
-							{
-									Log.log("sleep/wake in notificationReceived");
-								  Log.log("notification='"+notification+"'");
+	socketNotificationReceived: function(notification, payload)
+	{
+		Log.log("sleep/wake in notificationReceived");
+		Log.log("notification='"+notification+"'");
 
-									switch(notification) 
-									{	
-										case 'SLEEP_HIDE':
-											MM.getModules().enumerate((module) => {
-												 // if the module is already hidden
-												 if(module.hidden==true)
-														// save it for wake up
-														v_self1.previously_hidden.push(module.identifier)
-												 else
-														// hide this module
-														module.hide(1000);
-											});
-										break;
-										case 'SLEEP_WAKE':
-											 MM.getModules().enumerate((module) => {
-													// if this module was NOT in the previously hidden list
-													//Log.log("looking for module ="+module.name+" in previous list");
-													if(v_self1.previously_hidden.indexOf(module.identifier)==-1)
-													{														
-														// show it
-															module.show(1000);
-													}
-											 });
-											// clear the list, if any
-											v_self1.previously_hidden = [];
-										break;
-									}
-							},
-							PIR_Loaded: function(){
-								let rc=false;
-								MM.getModules().enumerate((module) => {
-									if(module.name=='MMM-PIR-Sensor'){
-											rc=true;
-											break;
-									}
-  							});
-								return rc;
-							},
-							notificationReceived: function(notification, payload, sender)
-							{
-									Log.log("sleep-wake in notificationReceived");
-								  Log.log("notification='"+notification+"'");
-									Log.log("sender="+sender);
-									//Log.log("payload="+JSON.stringify(payload));
-									switch(notification) 
-									{
-										case 'ALL_MODULES_STARTED':
-												v_self1=this;
-												if(v_self1.config.mode.toUpperCase()!=='PIR' || !this.PIR_Loaded())
-												{
-														//Log.log("SleepWake config="+JSON.stringify(v_self1.config));
-														v_self1.sendSocketNotification("config", v_self1.config);
-												}
-												else
-														Log.log("MMM-PIR-Sensor loaded, defering");
-										break;
-										case 'STAND_BY':
-											Log.log("received notification about sleep from "+ sender.name)
-											if(sender.name=='MMM-voice' || sender.name=='MMM-PIR-Sensor')
-												v_self1.sendSocketNotification(notification,payload);
-                      Log.log("config="+v_self1.config.mode.toUpperCase())
-											if(v_self1.config.mode.toUpperCase()==='HIDE' && payload.status === true){
-                        if( sender.name == 'MMM-AssistantMk2' && payload.triggerHide === true){
-                          //
-                          Log.log("MMM-AssistantMk2 says go to sleep");
-                          this.sendSocketNotification('START_SLEEP', null)
-                        }
-                        else {
-                          Log.log("previously hidden module identifiers="+payload.modules);
-                          // loop thru the modules
-                          MM.getModules().enumerate((module) => {
-                            // if this module should be in the previously hidden list
-                            if(payload.modules.indexOf(module.identifier) !== -1)
-                            {														
-                              // save it
-                              v_self1.previously_hidden.push(module.identifier)
-                            }
-                          })
-                        }
-											}
-										break;
-									}
-							},
-})
+		switch(notification)
+		{
+		case "SLEEP_HIDE":
+			MM.getModules().enumerate((module) => {
+						 // if the module is already hidden
+						 if(module.hidden==true)
+				// save it for wake up
+				{v_self1.previously_hidden.push(module.identifier);}
+						 else
+				// hide this module
+				{module.hide(1000);}
+			});
+			break;
+		case "SLEEP_WAKE":
+					 MM.getModules().enumerate((module) => {
+				// if this module was NOT in the previously hidden list
+				//Log.log("looking for module ="+module.name+" in previous list");
+				if(v_self1.previously_hidden.indexOf(module.identifier)==-1)
+				{
+					// show it
+					module.show(1000);
+				}
+					 });
+			// clear the list, if any
+			v_self1.previously_hidden = [];
+			break;
+		}
+	},
+	PIR_Loaded: function(){
+		let rc=false;
+		MM.getModules().enumerate((module) => {
+			if(module.name=="MMM-PIR-Sensor"){
+				rc=true;
+			}
+		});
+		return rc;
+	},
+	notificationReceived: function(notification, payload, sender)
+	{
+		Log.log("sleep-wake in notificationReceived");
+		Log.log("notification='"+notification+"'");
+		Log.log("sender="+sender);
+		//Log.log("payload="+JSON.stringify(payload));
+		switch(notification)
+		{
+		case "ALL_MODULES_STARTED":
+			v_self1=this;
+			if(v_self1.config.mode.toUpperCase()!=="PIR" || !this.PIR_Loaded())
+			{
+				//Log.log("SleepWake config="+JSON.stringify(v_self1.config));
+				v_self1.sendSocketNotification("config", v_self1.config);
+			}
+			else
+			{Log.log("MMM-PIR-Sensor loaded, defering");}
+			break;
+		case "STAND_BY":
+			Log.log("received notification about sleep from "+ sender.name);
+			if(sender.name=="MMM-voice" || sender.name=="MMM-PIR-Sensor")
+			{v_self1.sendSocketNotification(notification,payload);}
+			Log.log("config="+v_self1.config.mode.toUpperCase());
+			if(v_self1.config.mode.toUpperCase()==="HIDE" && payload.status === true){
+				if( sender.name == "MMM-AssistantMk2" && payload.triggerHide === true){
+					//
+					Log.log("MMM-AssistantMk2 says go to sleep");
+					this.sendSocketNotification("START_SLEEP", null);
+				}
+				else {
+					Log.log("previously hidden module identifiers="+payload.modules);
+					// loop thru the modules
+					MM.getModules().enumerate((module) => {
+						// if this module should be in the previously hidden list
+						if(payload.modules.indexOf(module.identifier) !== -1)
+						{
+							// save it
+							v_self1.previously_hidden.push(module.identifier);
+						}
+					});
+				}
+			}
+			break;
+		}
+	},
+});
--- node_helper.js
+++ node_helper.js
@@ -1,206 +1,206 @@
 var NodeHelper = require("node_helper");
-var fs = require('fs');
-var exec = require('child_process').exec;
-const path = require('path');
+var fs = require("fs");
+var exec = require("child_process").exec;
+const path = require("path");
 var vself=null;
 module.exports = NodeHelper.create({
 
-							config: null,
+	config: null,
 
-							timeractve : null,
-							sleeping: false,
-							hdmi:  false,
+	timeractve : null,
+	sleeping: false,
+	hdmi:  false,
 
-							init: function(){
-									console.log("handler helper in init");
-							},
-					
-							start: function(){
-									console.log("handler helper in start");
-									vself=this
-							},
-				
-							stop: function(){
-									console.log("handler helper in stop");
-							},
+	init: function(){
+		console.log("handler helper in init");
+	},
 
-							noUser: function()
-							{	
-								if(vself.sleeping==false)
-									vself.socketNotificationReceived('start_sleep');									
-							}		,
-							socketNotificationReceived: function(notification, payload)
+	start: function(){
+		console.log("handler helper in start");
+		vself=this;
+	},
+
+	stop: function(){
+		console.log("handler helper in stop");
+	},
+
+	noUser: function()
+	{
+		if(vself.sleeping==false)
+		{vself.socketNotificationReceived("start_sleep");}
+	}		,
+	socketNotificationReceived: function(notification, payload)
+	{
+		console.log("sleep-wake helper in socket notification="+notification);
+		switch(notification.toUpperCase())
+		{
+		case "CONFIG":
+			vself.config=payload;
+			vself.timeractive=setTimeout(vself.noUser,vself.config.delay*(60*1000));
+			if(vself.config.source.toUpperCase() === "EXTERNAL"){
+				exec("/bin/chmod +x "+path.join(__dirname,"external_motion"), function (error, stdout, stderr) {
+					if(error!=null)
+					{
+						console.log("change permissions failed "+JSON.stringify(error));
+					}
+				});
+
+				if(vself.config.detectionDir=="/motion")
+				{
+				  // its the default folder
+					//console.log("full path="+path.join(__dirname,vself.config.detectionDir))
+					vself.config.detectionDir= path.join(__dirname,vself.config.detectionDir);
+					//console.log("setting detectionDir path from local folder ="+vself.config.detectionDir);
+				}
+
+				//	console.log(" external source defined");
+
+				console.log(" external source defined dir="+vself.config.detectionDir);
+
+				// check to see if the external motion event folder exists
+				fs.access(vself.config.detectionDir, function(err) {
+					// if not
+					if (err && err.code === "ENOENT") {
+						// create it
+						fs.mkdir(vself.config.detectionDir);
+						console.log("created motion directory", vself.config.detectionDir);
+						exec("/bin/chmod 666 "+vself.config.detectionDir, function (error, stdout, stderr) {
+							if(error!=null)
 							{
-								console.log("sleep-wake helper in socket notification="+notification);
-								switch(notification.toUpperCase())
-								{
-									case 'CONFIG':
-										vself.config=payload;
+								console.log("change permissions failed "+JSON.stringify(error));
+							}
+						});
+					}
+					else{
+						// make sure the directory is empty
+						vself.rmDir(vself.config.detectionDir,false);
+					}
+					// change detector function
+					// watch for a file to appear in the folder
+					fs.watch(vself.config.detectionDir, (eventType, filename) => {
+						if (filename) {
+							// remove the file
+							//console.log(" f="+vself.config.detectionFile+" file="+filename+"\n")
+							//console.log("config="+JSON.stringify(vself.config));
+							fs.unlink(path.join(vself.config.detectionDir,filename), function(error) {
+								// consume the enonet error
+								if(error == null){
+									console.log("motion detected from external source");
+									// if the start motion file
+									if(filename === vself.config.detectionFile) {
+										// signal motion started
+										console.log("!s:","motionstart");
+										clearTimeout(vself.timeractive);
+										vself.timeractive=null;
+										if(vself.sleeping)
+										{vself.socketNotificationReceived("end_sleep");}
+									}
+									else {
+										// signal motion ended
+										console.log("!e:","motionend");
 										vself.timeractive=setTimeout(vself.noUser,vself.config.delay*(60*1000));
-										if(vself.config.source.toUpperCase() === 'EXTERNAL'){
-											exec('/bin/chmod +x '+path.join(__dirname,"external_motion"), function (error, stdout, stderr) {
-													if(error!=null)
-													{
-															console.log("change permissions failed "+JSON.stringify(error));
-													}
-											}); 
-
-											if(vself.config.detectionDir=='/motion')
-										  {
-												  // its the default folder
-													//console.log("full path="+path.join(__dirname,vself.config.detectionDir))
-													vself.config.detectionDir= path.join(__dirname,vself.config.detectionDir)
-													//console.log("setting detectionDir path from local folder ="+vself.config.detectionDir);
-											}
-
-//										  console.log(" external source defined");
-
-										  console.log(" external source defined dir="+vself.config.detectionDir);
-
-											// check to see if the external motion event folder exists
-											fs.access(vself.config.detectionDir, function(err) {
-												// if not
-												if (err && err.code === 'ENOENT') {
-													// create it
-													fs.mkdir(vself.config.detectionDir);
-													console.log('created motion directory', vself.config.detectionDir);
-													exec('/bin/chmod 666 '+vself.config.detectionDir, function (error, stdout, stderr) {
-													if(error!=null)
-													{
-															console.log("change permissions failed "+JSON.stringify(error));
-													}
-											});
-												}
-												else{
-													// make sure the directory is empty
-													vself.rmDir(vself.config.detectionDir,false);
-												}
-												// change detector function
-												// watch for a file to appear in the folder
-												fs.watch(vself.config.detectionDir, (eventType, filename) => {
-													if (filename) {
-														// remove the file
-														//console.log(" f="+vself.config.detectionFile+" file="+filename+"\n")
-														//console.log("config="+JSON.stringify(vself.config));
-														fs.unlink(path.join(vself.config.detectionDir,filename), function(error) { 
-															// consume the enonet error
-															if(error == null){
-																console.log('motion detected from external source');
-																// if the start motion file
-																if(filename === vself.config.detectionFile) {
-																	// signal motion started
-																	console.log("!s:","motionstart");
-																	clearTimeout(vself.timeractive)
-																	vself.timeractive=null;
-																	if(vself.sleeping)
-																		vself.socketNotificationReceived('end_sleep');
-																}
-																else {
-																	// signal motion ended 
-																	console.log("!e:","motionend");
-																	vself.timeractive=setTimeout(vself.noUser,vself.config.delay*(60*1000));
-																}
-															}
-														});
-													} else {		
-														console.log('filename not provided');
-													}
-												});
-											});
-										}
-										break;
-									case  'USER_PRESENCE':
-										if(payload==true)
-											vself.sleeping==false;
-										break;
-									case 'START_SLEEP':
-										vself.sleeping=true;
-										switch(vself.config.mode.toUpperCase()){
-											case 'PI':
-												exec('/opt/vc/bin/tvservice -o',  function (error, stdout, stderr) {
-													if(error!=null)
-													{
-															console.log("/opt/vc/bin/tvservice -o failed "+JSON.stringify(error));
-													}
-											});
-												vself.hdmi = false;
-												break;
-											case 'HIDE':
-												// tell the module so it can hide the others
-												vself.sendSocketNotification('SLEEP_HIDE');
-												break;
-											case 'DPMS':
-												/////////// Turns off laptop display and desktop PC with DVI  @ Mykle ///////////////
-												exec('xset dpms force off',  function (error, stdout, stderr) {
-													if(error!=null)
-													{
-															console.log("xset dpms force off failed "+JSON.stringify(error));
-													}
-											});
-												break;
-										}
-										if(vself.config.mode.toUpperCase()!=='HIDE')
-											vself.sendSocketNotification('HW_ASLEEP')
-										break;
-									case 'END_SLEEP':
-										vself.sleeping=false;
-										switch(vself.config.mode.toUpperCase())
-										{
-											case 'PI':
-												exec('/opt/vc/bin/tvservice -p && sudo chvt 6 && sudo chvt 7',  function (error, stdout, stderr) {
-													if(error!=null)
-													{
-															console.log("/opt/vc/bin/tvservice -p failed "+JSON.stringify(error));
-													}
-											});
-												vself.hdmi = true;
-												break;
-											case 'HIDE':
-												// tell the module so it can unhide the others
-												vself.sendSocketNotification('SLEEP_WAKE');
-												break;
-											case 'DPMS':
-												/////////// Turns on laptop display and desktop PC with DVI @ Mykle ///////////////
-												exec('xset dpms force on', function (error, stdout, stderr) {
-													if(error!=null)
-													{
-															console.log("xset dpms force on failed "+JSON.stringify(error));
-													}
-											});
-												break;
-										}
-										if(vself.config.mode.toUpperCase()!=='HIDE')
-											vself.sendSocketNotification('HW_AWAKE')
-										break;
-									case 'STAND_BY':
-										if(payload.status === false) {
-											vself.sleeping=false;
-											vself.timeractive=setTimeout(vself.noUser,vself.config.delay*(60*1000));
-										} else if (payload.status === true) {
-											vself.sleeping=true;
-											if(vself.timeractive)
-												clearTimeout(vself.timeractive)
-										}
-									break;
-									default:
-										break;
+									}
 								}
-							},
-		rmDir : function(dirPath, removevself) {
+							});
+						} else {
+							console.log("filename not provided");
+						}
+					});
+				});
+			}
+			break;
+		case  "USER_PRESENCE":
+			if(payload==true)
+			{vself.sleeping==false;}
+			break;
+		case "START_SLEEP":
+			vself.sleeping=true;
+			switch(vself.config.mode.toUpperCase()){
+			case "PI":
+				exec("/opt/vc/bin/tvservice -o",  function (error, stdout, stderr) {
+					if(error!=null)
+					{
+						console.log("/opt/vc/bin/tvservice -o failed "+JSON.stringify(error));
+					}
+				});
+				vself.hdmi = false;
+				break;
+			case "HIDE":
+				// tell the module so it can hide the others
+				vself.sendSocketNotification("SLEEP_HIDE");
+				break;
+			case "DPMS":
+				/////////// Turns off laptop display and desktop PC with DVI  @ Mykle ///////////////
+				exec("xset dpms force off",  function (error, stdout, stderr) {
+					if(error!=null)
+					{
+						console.log("xset dpms force off failed "+JSON.stringify(error));
+					}
+				});
+				break;
+			}
+			if(vself.config.mode.toUpperCase()!=="HIDE")
+			{vself.sendSocketNotification("HW_ASLEEP");}
+			break;
+		case "END_SLEEP":
+			vself.sleeping=false;
+			switch(vself.config.mode.toUpperCase())
+			{
+			case "PI":
+				exec("/opt/vc/bin/tvservice -p && sudo chvt 6 && sudo chvt 7",  function (error, stdout, stderr) {
+					if(error!=null)
+					{
+						console.log("/opt/vc/bin/tvservice -p failed "+JSON.stringify(error));
+					}
+				});
+				vself.hdmi = true;
+				break;
+			case "HIDE":
+				// tell the module so it can unhide the others
+				vself.sendSocketNotification("SLEEP_WAKE");
+				break;
+			case "DPMS":
+				/////////// Turns on laptop display and desktop PC with DVI @ Mykle ///////////////
+				exec("xset dpms force on", function (error, stdout, stderr) {
+					if(error!=null)
+					{
+						console.log("xset dpms force on failed "+JSON.stringify(error));
+					}
+				});
+				break;
+			}
+			if(vself.config.mode.toUpperCase()!=="HIDE")
+			{vself.sendSocketNotification("HW_AWAKE");}
+			break;
+		case "STAND_BY":
+			if(payload.status === false) {
+				vself.sleeping=false;
+				vself.timeractive=setTimeout(vself.noUser,vself.config.delay*(60*1000));
+			} else if (payload.status === true) {
+				vself.sleeping=true;
+				if(vself.timeractive)
+				{clearTimeout(vself.timeractive);}
+			}
+			break;
+		default:
+			break;
+		}
+	},
+	rmDir : function(dirPath, removevself) {
 		if (removevself === undefined)
-			removevself = true;
+		{removevself = true;}
 		try { var files = fs.readdirSync(dirPath); }
 		catch(e) { return; }
 		if (files.length > 0)
-			for (var i = 0; i < files.length; i++) {
-				var filePath = dirPath + '/' + files[i];
-				if (fs.statSync(filePath).isFile())
-					fs.unlinkSync(filePath);
-				else
-					rmDir(filePath);
-			}
+		{for (var i = 0; i < files.length; i++) {
+			var filePath = dirPath + "/" + files[i];
+			if (fs.statSync(filePath).isFile())
+			{fs.unlinkSync(filePath);}
+			else
+			{rmDir(filePath);}
+		}}
 		if (removevself)
-			fs.rmdirSync(dirPath);
+		{fs.rmdirSync(dirPath);}
 	},
 
 }
